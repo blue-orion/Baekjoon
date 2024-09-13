bba03f7141076421e9ff3d62338a684c97ec3a0b
Taeho <164999649+UnivTaeho@users.noreply.github.com>
2024-09-13 23:43:49 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d",&n);
+    if (n < 0 || n > 100000)
+        return 0;
+
+    for (int i = 0; i < n; i++) {
+        printf("%d\n",i+1);
+    }
+}
