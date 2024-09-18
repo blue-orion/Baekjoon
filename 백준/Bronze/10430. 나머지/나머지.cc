d693e340051d19b59c80da8393cde52ca302362c
Taeho <164999649+UnivTaeho@users.noreply.github.com>
2024-09-18 23:51:22 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << (a + b) % c << endl;
+	cout << ((a % c) + (b % c)) % c << endl;
+	cout << (a * b) % c << endl;
+	cout << ((a % c) * (b % c)) % c;
+	return (0);
+}
